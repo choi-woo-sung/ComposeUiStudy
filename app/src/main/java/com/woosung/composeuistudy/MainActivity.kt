de981e62e21d377d1addeb7e2aed0e9119d5100f
최우성 <63629853+choi-woo-sung@users.noreply.github.com>
2023-07-11 19:00:19 +0900

    Initial commit

---
+++ app/src/main/java/com/woosung/composeuistudy/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.woosung.composeuistudy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.woosung.composeuistudy.ui.theme.ComposeUIStudyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeUIStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+//                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageSlider(images: List<Any>) {
+    var currentImageIndex by remember { mutableStateOf(0) }
+    val coroutineScope = rememberCoroutineScope()
+    val state = rememberLazyListState()
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(100.dp)
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            // Scrollable Row of Cards
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                state = state,
+
+            ) {
+                itemsIndexed(images) { index, image ->
+                    CaroselCard(image)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CaroselCard(image: Any ) {
+    val width by remember { mutableStateOf(300.dp) }
+    Card(
+        modifier = Modifier
+            .width(300.dp)
+            .height(200.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .width(width)
+                .height(200.dp),
+            model = image as String,
+            contentDescription = "",
+
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    val images = listOf(
+        "https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg",
+        "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__480.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrfPnodZbEjtJgE-67C-0W9pPXK8G9Ai6_Rw&usqp=CAU",
+        "https://i.ytimg.com/vi/E9iP8jdtYZ0/maxresdefault.jpg",
+        "https://cdn.shopify.com/s/files/1/0535/2738/0144/articles/shutterstock_149121098_360x.jpg",
+    )
+    ComposeUIStudyTheme {
+        ImageSlider(images)
+    }
+}
